945ebd5bd27d942a2bbd99ddf531298c0cd554e1
ayerest <irisiseve@gmail.com>
2019-08-14 17:06:46 -0700

    finished activerecord validations lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait
+
+    def clickbait
+        clickbait_words = [
+            /Won't Believe/i,
+            /Secret/i,
+            /Top [0-9]*/i,
+            /Guess/i
+        ]
+        if !clickbait_words.any? {|clickbait| clickbait.match(title)}
+            errors.add(:title, "Title needs to include clickbait")
+        end            
+    end
 end
